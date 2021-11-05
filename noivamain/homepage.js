27115eaccf661700a748fb1ee14413e029ea2dd6
meimei-xu <nchiu1@ocdsb.ca>
2021-11-04 23:13:34 -0400

    lots of changes made, will explain

---
+++ noivamain/homepage.js
@@ -0,0 +1,11 @@
+// Opening & closing sidebar functions
+
+// Open sidebar 
+function openNav() {
+    document.getElementById("mySidebar").style.width = "60%";
+}
+
+// Close/hide sidebar 
+function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+}
