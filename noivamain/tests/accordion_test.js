00f5f0cea676e8e4253c7c2c2f2dd1f1fd34d96c
meimei-xu <nchiu1@ocdsb.ca>
2021-11-15 17:43:26 -0500

    Added image of logo and accordion progress

--- noivamain/tests/accordion_test.js
+++ noivamain/tests/accordion_test.js
@@ -16,5 +16,14 @@ const accordionItemHeaders = document.querySelectorAll(".accordion-item-header")
 accordionItemHeaders.forEach(accordionItemHeader => {
     accordionItemHeader.addEventListener("click", event => {
         accordionItemHeader.classList.toggle("active");
+
+        // Opening and closing the accordion
+        const accordionItemBody = accordionItemHeader.nextElementSibling;
+        if(accordionItemHeader.classList.contains("active")) {
+            accordionItemBody.style.maxHeight = accordionItemBody.scrollHeight + "px";
+        }
+        else {
+            accordionItemBody.style.maxHeight = "0";
+        }
     });
 });
