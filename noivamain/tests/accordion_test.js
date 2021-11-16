24c9c8b36e4aa03da7f0f084327da17b3ca3f0e9
meimei-xu <nchiu1@ocdsb.ca>
2021-11-16 17:49:20 -0500

    Completed music function, uploaded music

--- noivamain/tests/accordion_test.js
+++ noivamain/tests/accordion_test.js
@@ -29,18 +29,111 @@ accordionItemHeaders.forEach(accordionItemHeader => {
 });
 
 // Playing music and changing the icon
+
+// Assigning variables to the songs
 var songOne = document.getElementById("songOne");
 var songTwo = document.getElementById("songTwo");
+var songThree = document.getElementById("songThree");
+var songFour = document.getElementById("songFour");
 
-var musicIcon = document.getElementById("musicIcon");
+// Assigning variables to the music icons to toggle between play and pause
+var musicIconOne = document.getElementById("musicIconOne");
+var musicIconTwo = document.getElementById("musicIconTwo")
+var musicIconThree = document.getElementById("musicIconThree")
+var musicIconFour = document.getElementById("musicIconFour")
 
-musicIcon.onclick = function() {
+// Playing and pausing song one
+musicIconOne.onclick = function() {
     if (songOne.paused) {
+        // Song one plays
         songOne.play();
-        musicIcon.src = '/images/pausebutton.png';
+
+        // All other songs pause
+        songTwo.pause();
+        songThree.pause();
+        songFour.pause();
+
+        // Song one icon changes to pause
+        musicIconOne.src = '/images/pausebutton.png';
+
+        // All other song icons change to play
+        musicIconTwo.src = '/images/playbutton.png';
+        musicIconThree.src = '/images/playbutton.png';
+        musicIconFour.src = '/images/playbutton.png';
     } else {
         songOne.pause();
-        musicIcon.src = '/images/playbutton.png';
+        musicIconOne.src = '/images/playbutton.png';
     }
 }
 
+// Playing and pausing song two
+musicIconTwo.onclick = function() {
+    if (songTwo.paused) {
+        // Song two plays
+        songTwo.play();
+
+        // All other songs pause
+        songOne.pause();
+        songThree.pause();
+        songFour.pause();
+
+        // Song two icon changes to pause
+        musicIconTwo.src = '/images/pausebutton.png';
+
+        // All other song icons change to play
+        musicIconOne.src = '/images/playbutton.png';
+        musicIconThree.src = '/images/playbutton.png';
+        musicIconFour.src = '/images/playbutton.png';
+    } else {
+        songTwo.pause();
+        musicIconTwo.src = '/images/playbutton.png';
+    }
+}
+
+// Playing and pausing song three
+musicIconThree.onclick = function() {
+    if (songThree.paused) {
+        // Song three plays
+        songThree.play();
+
+        // All other songs pause
+        songOne.pause();
+        songTwo.pause();
+        songFour.pause();
+
+        // Song three icon changes to pause
+        musicIconThree.src = '/images/pausebutton.png';
+
+        // All other song icons change to play
+        musicIconOne.src = '/images/playbutton.png';
+        musicIconTwo.src = '/images/playbutton.png';
+        musicIconFour.src = '/images/playbutton.png';
+    } else {
+        songThree.pause();
+        musicIconThree.src = '/images/playbutton.png';
+    }
+}
+
+// Playing and pausing song four
+musicIconFour.onclick = function() {
+    if (songFour.paused) {
+        // Song four plays
+        songFour.play();
+
+        // All other songs pause
+        songOne.pause();
+        songTwo.pause();
+        songThree.pause();
+
+        // Song four icon changes to pause
+        musicIconFour.src = '/images/pausebutton.png';
+
+        // All other song icons change to play
+        musicIconOne.src = '/images/playbutton.png';
+        musicIconTwo.src = '/images/playbutton.png';
+        musicIconThree.src = '/images/playbutton.png';
+    } else {
+        songFour.pause();
+        musicIconFour.src = '/images/playbutton.png';
+    }
+}
