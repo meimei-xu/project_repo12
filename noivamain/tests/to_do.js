1ee6578275ccc9cdd883c3c8b69c47b32dc414ab
meimei-xu <nchiu1@ocdsb.ca>
2021-11-18 15:56:19 -0500

    Copy and pasta to-do function from online

--- noivamain/tests/to_do.js
+++ noivamain/tests/to_do.js
@@ -1,55 +1,202 @@
-// Create a "close" button and append it to each list item
-var myNodelist = document.getElementsByTagName("LI");
-var i;
-for (i = 0; i < myNodelist.length; i++) {
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  myNodelist[i].appendChild(span);
+const dateElement = document.getElementById('date');
+const clear = document.querySelector('.clear');
+const list = document.getElementById('list');
+const input = document.querySelector('input');
+const addButton = document.querySelector('.fa-plus-circle');
+const show_all = document.querySelector('.All');
+const show_finished = document.querySelector('.fulfill');
+const show_unfinished = document.querySelector('.unfinished');
+
+const CHECK = "fa-check-circle";
+const UNCHECK = "fa-circle-thin";
+const LINE_THROUGH = "lineThrough";
+const CHANGE = "editable";
+
+var status = "all", timer = null, delay = 260, click = 0;//double click
+
+clear.addEventListener("click", function () {
+    localStorage.clear();
+    location.reload();
+});
+/* show time */
+const options = { weekday: 'long', month: 'short', day: 'numeric' };
+const today = new Date();
+dateElement.innerHTML = today.toLocaleDateString("en-US", options);
+
+let LIST, id;
+let data = localStorage.getItem("TODO");
+//get historical data from localstorage
+if (data) {
+    LIST = JSON.parse(data);
+    id = LIST.length;
+    loadList(LIST);
+}
+else {
+    LIST = []
+    id = 0;
 }
 
-// Click on a close button to hide the current list item
-var close = document.getElementsByClassName("close");
-var i;
-for (i = 0; i < close.length; i++) {
-  close[i].onclick = function() {
-    var div = this.parentElement;
-    div.style.display = "none";
-  }
+function loadList(array) {
+    array.forEach(element => {
+        add_to_do(element.name, element.id, element.done, element.trash);
+    });
 }
 
-// Add a "checked" symbol when clicking on a list item
-var list = document.querySelector('ul');
-list.addEventListener('click', function(ev) {
-  if (ev.target.tagName === 'LI') {
-    ev.target.classList.toggle('checked');
-  }
+function add_to_do(toDo, id, done, trash) {
+    if (trash)
+        return;
+    const DONE = done ? CHECK : UNCHECK;
+    const LINE = done ? LINE_THROUGH : "";
+
+    const item = `<li class="item">
+                    <i class="fa ${DONE} co" job="complete" id="${id}"></i>
+                    <input disabled="disabled" class="text ${LINE}" job="edit" value="${toDo}" id="${id}" ">
+                    <i class="fa fa-trash-o de" job="delete" id="${id}"></i>
+                </li>
+                `;
+    const position = "beforeend";
+    list.insertAdjacentHTML(position, item);//可以使用appendchild直接插入dom，效率会更高
+}
+
+function defReload(array, status){
+    if(status === "all"){
+        array.forEach(element=>{
+            add_to_do(element.name, element.id, element.done, element.trash)
+        });
+    }
+    else if(status === "completed"){
+        array.forEach(element => {
+            if (element.done) {
+                add_to_do(element.name, element.id, element.done, element.trash);
+            }
+        });
+    }
+    else{
+        array.forEach(element => {
+            if (!element.done) {
+                add_to_do(element.name, element.id, element.done, element.trash);
+            }
+        });
+    }
+}
+
+show_all.addEventListener("click", function () {
+    var i, list_length = list.childNodes.length;
+    status = "all";
+    for (i = 0; i < list_length; i++) {
+        list.removeChild(list.lastChild);
+    }
+    defReload(LIST,"all");
+});
+
+show_finished.addEventListener("click", function () {
+    var i, list_length = list.childNodes.length;
+    status = "completed";
+    for (i = 0; i < list_length; i++) {
+        list.removeChild(list.lastChild);
+    }
+    defReload(LIST,"completed");
+});
+
+show_unfinished.addEventListener("click", function () {
+    var i, list_length = list.childNodes.length;
+    status = "active";
+    for (i = 0; i < list_length; i++) {
+        list.removeChild(list.lastChild);
+    }
+    defReload(LIST,"active");
+});
+
+input.addEventListener("keypress", function (even) {
+    if (event.keyCode === 13) {
+        const toDo = input.value;
+        if (toDo) {
+            add_to_do(toDo, id, false, false);
+
+            LIST.push({ name: toDo, id: id, done: false, trash: false });
+
+            localStorage.setItem("TODO", JSON.stringify(LIST));
+            id++;
+            input.value = "";
+        }
+    }
+});
+
+addButton.addEventListener('click', function () {
+    const toDo = input.value;
+    if (toDo) {
+        add_to_do(toDo, id, false, false);
+
+        LIST.push({ name: toDo, id: id, done: false, trash: false });
+
+        localStorage.setItem("TODO", JSON.stringify(LIST));
+        id++;
+        input.value = "";
+    }
+});
+
+list.addEventListener("click", function (event) {
+    const element = event.target;
+    const elementJob = element.attributes.job.value;
+
+    if (elementJob === 'complete') {
+        completeToDo(element);
+    } else if (elementJob === 'delete') {
+        removeToDo(element);
+    }
+    else if (elementJob === 'edit') {
+        editTodo(element);
+    }
+
+    localStorage.setItem("TODO", JSON.stringify(LIST));
+
 }, false);
 
-// Create a new list item when clicking on the "Add" button
-function newElement() {
-  var li = document.createElement("li");
-  var inputValue = document.getElementById("myInput").value;
-  var t = document.createTextNode(inputValue);
-  li.appendChild(t);
-  if (inputValue === '') {
-    alert("You must write something!");
-  } else {
-    document.getElementById("myUL").appendChild(li);
-  }
-  document.getElementById("myInput").value = "";
+function completeToDo(element) {
+    element.classList.toggle(UNCHECK);
+    element.classList.toggle(CHECK);
+    element.parentNode.querySelector(".text").classList.toggle(LINE_THROUGH);
 
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  li.appendChild(span);
-
-  for (i = 0; i < close.length; i++) {
-    close[i].onclick = function() {
-      var div = this.parentElement;
-      div.style.display = "none";
+    LIST[element.id].done = LIST[element.id].done ? false : true;
+    if(status === "active" && LIST[element.id].done){
+        element.parentNode.parentNode.removeChild(element.parentNode);
     }
-  }
+    else if(status === "completed" && !LIST[element.id].done){
+        element.parentNode.parentNode.removeChild(element.parentNode);
+    }
+    else if(LIST[element.id].done){
+        var temp = element.parentNode, realParent = element.parentNode.parentNode;
+        element.parentNode.parentNode.removeChild(element.parentNode);
+        realParent.appendChild(temp);
+    }
+}
+
+function removeToDo(element) {
+    element.parentNode.parentNode.removeChild(element.parentNode);
+    LIST[element.id].trash = true;
+}
+
+function editTodo(element) {
+    var newTodo, content = element.parentNode.querySelector(".text"), preText;
+    click++;
+    if (click === 1) {
+        timer = setTimeout(function () {
+            click = 0;
+        }, (delay));
+    } else {
+        click = 0;
+        clearTimeout(timer);
+        preText = content.value;
+        content.disabled = false;
+        content.focus();
+        content.onblur = () => {
+            newTodo = content.value;
+            if (preText !== newTodo) {
+                LIST[element.id].name = newTodo;
+                localStorage.setItem("TODO", JSON.stringify(LIST));
+            }
+            content.disabled = true;
+        };
+    }
+
 }
