d523cff2d71ee35f7aa0fa9496539be522a7024a
meimei-xu <nchiu1@ocdsb.ca>
2021-12-03 09:22:53 -0500

    Potential Goals list

--- noivamain/tests/to_do.js
+++ noivamain/tests/to_do.js
@@ -1,104 +1,110 @@
-// Add note to local storage
-let addBtn = document.getElementById("add-btn");
-addBtn.addEventListener("click", function(e) {
-
-  let addTitle = document.getElementById("note-title");
-  let addTxt = document.getElementById("note-text");
-  
-    if (addTitle.value == "" || addTxt.value == "") {
-        return alert("Please add Note Title and Details")
-    }
-
-  let notes = localStorage.getItem("notes");
-  if (notes == null) {
-    notesObj = [];
-  } else {
-    notesObj = JSON.parse(notes);
-  }
-  let myObj = {
-    title: addTitle.value,
-    text: addTxt.value
-  }
-  notesObj.push(myObj);
-  localStorage.setItem("notes", JSON.stringify(notesObj));
-  addTxt.value = "";
-  addTitle.value = "";
-//   console.log(notesObj);
-  showNotes();
-});
-
-// Function to show elements from localStorage
-function showNotes() {
-  let notes = localStorage.getItem("notes");
-  if (notes == null) {
-    notesObj = [];
-  } else {
-    notesObj = JSON.parse(notes);
-  }
-  let html = "";
-  notesObj.forEach(function(element, index) {
-    html += `
-        <div class="note">
-            <p class="note-counter">Note ${index + 1}</p>
-            <h3 class="note-title"> ${element.title} </h3>
-            <p class="note-text"> ${element.text}</p>
-            <button id="${index}"onclick="deleteNote(this.id)" class="note-btn">Delete Note</button>
-            <button id="${index}"onclick="editNote(this.id)" class="note-btn edit-btn">Edit Note</button>
-        </div>
-            `;
-  });
-  let notesElm = document.getElementById("notes");
-  if (notesObj.length != 0) {
-    notesElm.innerHTML = html;
-  } else {
-    notesElm.innerHTML = `No Notes Yet! Add a note using the form above.`;
-  }
+const noteListDiv = document.querySelector('.note-list');
+let noteID = 1;
+function Note(id, title, content){
+    this.id = id;
+    this.title = title;
+    this.content = content;
 }
 
-// Function to delete a note
-function deleteNote(index) {
-//   console.log("I am deleting", index);
-    let confirmDel = confirm("Delete this note?");
-    if (confirmDel == true) {
-        let notes = localStorage.getItem("notes");
-        if (notes == null) {
-            notesObj = [];
-        } else {
-            notesObj = JSON.parse(notes);
-        }
-
-        notesObj.splice(index, 1);
-        localStorage.setItem("notes", JSON.stringify(notesObj));
-        showNotes();
-    }
-  
+// all eventlisteners
+function eventListeners(){
+    document.addEventListener('DOMContentLoaded', displayNotes);
+    document.getElementById('add-note-btn').addEventListener('click', addNewNote);
+    noteListDiv.addEventListener('click', deleteNote);
+    document.getElementById('delete-all-btn').addEventListener('click', deleteAllNotes);
 }
 
-// Function to Edit the Note
-function editNote(index) {
-    let notes = localStorage.getItem("notes");
-    let addTitle = document.getElementById("note-title");
-    let addTxt = document.getElementById("note-text");
+eventListeners();
 
-    if (addTitle.value !== "" || addTxt.value !== "") {
-      return alert("Please clear the form before editing a note")
-    } 
+// get items form storage
+function getDataFromStorage(){
+    return localStorage.getItem('notes') ? JSON.parse(localStorage.getItem('notes')) : [];
+}
 
-    if (notes == null) {
-      notesObj = [];
+// add a new note in the list
+function addNewNote(){
+    const noteTitle = document.getElementById('note-title'),
+          noteContent = document.getElementById('note-content');
+    if(validateInput(noteTitle, noteContent)){
+        let notes = getDataFromStorage();
+        let noteItem = new Note(noteID, noteTitle.value, noteContent.value);
+        noteID++;
+        notes.push(noteItem);
+        createNote(noteItem);
+        // saving in the local storage
+        localStorage.setItem('notes', JSON.stringify(notes));
+        noteTitle.value = "";
+        noteContent.value = "";
+    }
+}
+
+// input validation
+function validateInput(title, content){
+    if(title.value !== "" && content.value !== ""){
+        return true;
     } else {
-      notesObj = JSON.parse(notes);
+        if(title.value === "") title.classList.add('warning');
+        if(content.value === "") content.classList.add('warning');
     }
-    console.log(notesObj);
+    setTimeout(() => {
+        title.classList.remove('warning');
+        content.classList.remove('warning');
+    }, 1500);
+}
 
-    notesObj.findIndex((element, index) => {
-      addTitle.value = element.title;
-      addTxt.value = element.text;
-    })
-    notesObj.splice(index, 1);
-        localStorage.setItem("notes", JSON.stringify(notesObj));
-        showNotes();
+// create a new note div
+function createNote(noteItem){
+    const div = document.createElement('div');
+    div.classList.add('note-item');
+    div.setAttribute('data-id', noteItem.id);
+    div.innerHTML = `
+        <h3>${noteItem.title}</h3>
+        <p>${noteItem.content}</p>
+        <button type = "button" class = "btn delete-note-btn">
+        <span><i class = "fas fa-trash"></i></span>
+        Remove
+        </button>
+    `;
+    noteListDiv.appendChild(div);
 }
 
 
-showNotes();
+// display all the notes form the local storage
+function displayNotes(){
+    let notes = getDataFromStorage();
+    if(notes.length > 0){
+        noteID = notes[notes.length - 1].id;
+        noteID++;
+    } else {
+        noteID = 1;
+    }
+    notes.forEach(item => {
+        createNote(item);
+    });
+}
+
+// delete a note 
+function deleteNote(e){
+    if(e.target.classList.contains('delete-note-btn')){
+        //console.log(e.target.parentElement);
+        e.target.parentElement.remove(); // removing from DOM
+        let divID = e.target.parentElement.dataset.id;
+        let notes = getDataFromStorage();
+        let newNotesList = notes.filter(item => {
+            return item.id !== parseInt(divID);
+        });
+        localStorage.setItem('notes', JSON.stringify(newNotesList));
+    }
+}
+
+// delete all notes
+function deleteAllNotes(){
+    localStorage.removeItem('notes');
+    let noteList = document.querySelectorAll('.note-item');
+    if(noteList.length > 0){
+        noteList.forEach(item => {
+            noteListDiv.removeChild(item);
+        });
+    }
+    noteID = 1; // resetting noteID to 1
+}
