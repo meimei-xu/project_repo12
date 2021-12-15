83b8c41da2c2d88d2f45c81e87061cedd85c41c7
meimei-xu <nchiu1@ocdsb.ca>
2021-12-15 16:05:42 -0500

    database practice 1.0

--- noivamain/database/database.js
+++ noivamain/database/database.js
@@ -1,9 +1,9 @@
-'use strict';
+
 
 const fs = require('fs');
 
 let student = {
-    name: 'Linde',
+    name: 'Linda',
     age: 64, 
     gender: 'Female',
     department: 'French',
@@ -12,7 +12,7 @@ let student = {
 
 let data = JSON.stringify(student, null, 2);  
 
-fs.writeFileSync('database.json', data, finished);
+fs.writeFileSync('./noivamain/database/database.json', data, finished);
 
 function finished(err)
 {
