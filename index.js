1c8e14c85a5526ca68842c3a1ab2fd3d8494db82
Ally Guo <69687800+ally-223@users.noreply.github.com>
2022-01-11 10:07:10 -0500

    trying

--- index.js
+++ index.js
@@ -5,7 +5,23 @@ const app = express();
 //getting the html
 app.use("/public", express.static(path.join(__dirname, "public")));
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "official_homepage.html"));
+  res.sendFile(path.join(__dirname, "public", "userlogin", "signup.html"));
 });
 
-app.listen(3002, () => console.log("listening at port 3002"));
+app.listen(3002, (err) => {
+  if (err) {
+    return console.log("Error", err);
+  }
+  console.log("listening at port 3002");
+});
+
+//actions on the server side code
+//api = application programming interface. api for client to send data to me
+//function has two arguments, request & response
+//request: holds all data being sent
+//response: sending things back to the client
+
+app.post("/api", (req, res) => {
+  console.log("hello");
+  console.log(req.body);
+});
