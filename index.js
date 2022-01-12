caa6631b1628c8f11f83b95aad56a57df8600d2f
Ally Guo <69687800+ally-223@users.noreply.github.com>
2022-01-12 11:50:44 -0500

    login function

--- index.js
+++ index.js
@@ -1,16 +1,37 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
 const express = require("express");
 const path = require("path");
 const app = express();
 const bcrypt = require("bcrypt");
 const passport = require("passport");
+const flash = require("express-flash");
+const session = require("express-session");
 
 const initializePassport = require("./passport-config");
-initializePassport(passport);
+initializePassport(
+  passport,
+  (name) => users.find((user) => user.name === name),
+  (id) => users.find((user) => user.id === id)
+);
 
 const users = [];
 
 app.set("view-engine", "ejs");
 app.use(express.urlencoded({ extended: false }));
+app.use(flash());
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 //getting the static files
 app.use("/public", express.static(path.join(__dirname, "public")));
@@ -26,7 +47,14 @@ app.get("/", (req, res) => {
 app.get("/login", (req, res) => {
   res.render("login.ejs");
 });
-app.post("/login", (req, res) => {});
+app.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/login",
+    failureFlash: true,
+  })
+);
 
 //signup get and post
 app.get("/signup", (req, res) => {
@@ -34,12 +62,12 @@ app.get("/signup", (req, res) => {
 });
 app.post("/signup", async (req, res) => {
   try {
-    const pass = await bcrypt.hash(req.body.pw, 10);
+    const pass = await bcrypt.hash(req.body.password, 10);
     users.push({
       id: Date.now().toString(),
       name: req.body.name,
       password: pass,
-      email: req.body.mail,
+      email: req.body.email,
     });
     res.redirect("/login");
   } catch {
@@ -60,8 +88,3 @@ app.listen(3002, (err) => {
 //function has two arguments, request & response
 //request: holds all data being sent
 //response: sending things back to the client
-
-app.post("/api", (req, res) => {
-  console.log("hello");
-  console.log(req.body);
-});
--- passport-config.js
+++ passport-config.js
@@ -1,7 +1,27 @@
 const LocalStrategy = require("passport-local").Strategy;
+const bcrypt = require("bcrypt");
 
-function initialize(passport) {
-  const authenticateUser = (name, pw, done) => {};
-  passport.use(new LocalStrategy({ usernameField: "name" }), authenticateUser);
+function initialize(passport, getUserByName) {
+  const authenticateUser = async (name, password, done) => {
+    const user = getUserByName(name);
+    if (user == null) {
+      return done(null, false, { message: "No user with this username" });
+    }
+
+    try {
+      if (await bcrypt.compare(password, user.password)) {
+        return done(null, user);
+      } else {
+        return done(null, false, { message: "Wrong Password" });
+      }
+    } catch (e) {
+      return done(e);
+    }
+  };
+
+  passport.use(new LocalStrategy({ usernameField: "name" }, authenticateUser));
   passport.serializeUser((user, done) => {});
+  passport.deserializeUser((id, done) => {});
 }
+
+module.exports = initialize;
