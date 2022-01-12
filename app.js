3fe2abd2079f8c01aaa677fafd75530938335576
meimei-xu <nchiu1@ocdsb.ca>
2022-01-11 22:59:46 -0500

    progress on applying learning to our code

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log('Hello World');
+const express = require('express');
+const path = require('path');
+
+// Init App
+const app = express();
+
+app.get('/', function(req, res){
+    res.send('Hello World');
+});
+
+//Start Server
+app.listen(3000, function(){
+    console.log('Server started on port 3000...')
+});
