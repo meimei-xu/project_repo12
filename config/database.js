b7fec6b1e6ec0fac4d742559dd9a58bd88a62c87
meimei-xu <nchiu1@ocdsb.ca>
2022-01-13 09:23:11 -0500

    changes

--- config/database.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    database: 'mongodb+srv://arial565:TryingToCreateADatabase20220101@project-database.mykuq.mongodb.net/official_database?retryWrites=true&w=majority',
-    secret: 'yoursecret'
-}
